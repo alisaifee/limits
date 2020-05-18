8827a14c8607f92762769adb4ddcea1fede471a5
Ali-Akber Saifee <ali@indydevs.org>
2020-05-18 16:59:29 -0700

    Lint setup.py

--- setup.py
+++ setup.py
@@ -31,5 +31,5 @@ setup(
     classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
     description='Rate limiting utilities',
     long_description=open('README.rst').read(),
-    packages=find_packages(exclude=['google.*','google', 'tests*']),
+    packages=find_packages(exclude=['google.*', 'google', 'tests*']),
 )
