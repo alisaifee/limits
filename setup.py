23e3c235badb6f2c4f40fff1999503c828d974a1
Ali-Akber Saifee <ali@indydevs.org>
2021-12-16 15:10:23 -0800

    Configure setup.py to use extras

--- setup.py
+++ setup.py
@@ -7,18 +7,33 @@ __author__ = "Ali-Akber Saifee"
 __email__ = "ali@indydevs.org"
 __copyright__ = "Copyright 2015, Ali-Akber Saifee"
 
+import itertools
 import os
 from setuptools import setup, find_packages
 import versioneer
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-REQUIREMENTS = [
-    k
-    for k in open(os.path.join(THIS_DIR, "requirements", "main.txt"))
-    .read()
-    .splitlines()
-    if k.strip()
-]
+
+
+def get_requirements(req_file):
+    requirements = []
+
+    for r in open(os.path.join(THIS_DIR, "requirements", req_file)).read().splitlines():
+        if r.strip():
+            requirements.append(r.strip())
+
+    return requirements
+
+
+EXTRA_REQUIREMENTS = {
+    "redis": get_requirements("storage/redis.txt"),
+    "rediscluster": get_requirements("storage/rediscluster.txt"),
+    "memcached": get_requirements("storage/memcached.txt"),
+    "mongodb": get_requirements("storage/mongodb.txt"),
+    "async-redis": get_requirements("storage/async-redis.txt"),
+    "async-memcached": get_requirements("storage/async-memcached.txt"),
+}
+EXTRA_REQUIREMENTS["all"] = list(itertools.chain(*EXTRA_REQUIREMENTS.values()))
 
 setup(
     name="limits",
@@ -29,12 +44,13 @@ setup(
     zip_safe=False,
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    install_requires=REQUIREMENTS,
+    install_requires=get_requirements("main.txt"),
     classifiers=[k for k in open("CLASSIFIERS").read().split("\n") if k],
     description="Rate limiting utilities",
     long_description=open("README.rst").read(),
     packages=find_packages(exclude=["google.*", "google", "tests*"]),
     python_requires=">3.7",
+    extras_require=EXTRA_REQUIREMENTS,
     package_data={
         "limits": ["py.typed"],
     },
