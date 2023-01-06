150bc8c3115c92e4bef1849323df71adfe5e978b
Ali-Akber Saifee <ali@indydevs.org>
2023-01-06 15:56:41 -0800

    Add basic benchmarks

---
+++ tests/benchmarks/test_storage.py
@@ -0,0 +1,75 @@
+import uuid
+import functools
+
+import pytest
+
+from limits import RateLimitItemPerMinute
+from limits.storage import storage_from_string
+from limits.strategies import FixedWindowRateLimiter, MovingWindowRateLimiter
+from tests.utils import (
+    all_storage,
+    moving_window_storage,
+    async_all_storage,
+    async_moving_window_storage,
+)
+import limits.aio.strategies
+
+
+def hit_window(strategy, storage):
+    limit = RateLimitItemPerMinute(500)
+    uid = uuid.uuid4().hex
+    strategy(storage).hit(limit, uid)
+
+
+def hit_window_async(event_loop, strategy, storage):
+    limit = RateLimitItemPerMinute(500)
+    uid = uuid.uuid4().hex
+    event_loop.run_until_complete(strategy(storage).hit(limit, uid))
+
+
+@all_storage
+@pytest.mark.benchmark
+def test_fixed_window(benchmark, uri, args, fixture):
+    benchmark(
+        functools.partial(
+            hit_window, FixedWindowRateLimiter, storage_from_string(uri, **args)
+        )
+    )
+
+
+@moving_window_storage
+@pytest.mark.benchmark
+def test_moving_window(benchmark, uri, args, fixture):
+    benchmark(
+        functools.partial(
+            hit_window, MovingWindowRateLimiter, storage_from_string(uri, **args)
+        )
+    )
+
+
+@async_all_storage
+@pytest.mark.asyncio
+@pytest.mark.benchmark
+def test_fixed_window_async(event_loop, benchmark, uri, args, fixture):
+    benchmark(
+        functools.partial(
+            hit_window_async,
+            event_loop,
+            limits.aio.strategies.FixedWindowRateLimiter,
+            storage_from_string(uri, **args),
+        )
+    )
+
+
+@async_moving_window_storage
+@pytest.mark.asyncio
+@pytest.mark.benchmark
+def test_moving_window_async(event_loop, benchmark, uri, args, fixture):
+    benchmark(
+        functools.partial(
+            hit_window_async,
+            event_loop,
+            limits.aio.strategies.MovingWindowRateLimiter,
+            storage_from_string(uri, **args),
+        )
+    )
