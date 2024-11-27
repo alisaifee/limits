a3f3d1fc752e54bef1472818fa59b869a67375bc
Ali-Akber Saifee <ali@indydevs.org>
2024-11-27 13:20:29 -0800

    Update CI to use python 3.9+ only

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     description="Rate limiting utilities",
     long_description=open("README.rst").read(),
     packages=find_packages(exclude=["tests*"]),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     extras_require=EXTRA_REQUIREMENTS,
     include_package_data=True,
     package_data={
