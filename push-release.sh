49cf77333ccb70b4521fa2d44792b38d7ccc6efb
Ali-Akber Saifee <ali@indydevs.org>
2025-03-14 20:23:25 -0700

    Update release script for 4.x

--- push-release.sh
+++ push-release.sh
@@ -1,8 +1,8 @@
-#!/bin/bash 
+#!/bin/bash
 cur=$(git rev-parse --abbrev-ref HEAD)
-git checkout master 
-git push origin master --tags 
+git checkout 4.x
+git push origin 4.x --tags
 git checkout stable
-git merge master
+git merge 4.x
 git push origin stable
 git checkout $cur
